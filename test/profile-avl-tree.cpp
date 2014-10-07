b15e76e325faedcbef29d9ae84b4ec7a96476e4a
Alex Nichol <unixpickle@gmail.com>
2014-10-07 06:17:31 -0400

    simple profiler for add/remove

--- test/profile-avl-tree.cpp
+++ test/profile-avl-tree.cpp
@@ -3,6 +3,7 @@
 #include <ansa/numeric-info>
 #include "nanotime.hpp"
 #include "posix-virtual-aligner.hpp"
+#include "stack-allocator.hpp"
 
 using namespace ansa;
 using namespace analloc;
@@ -10,12 +11,22 @@ using namespace analloc;
 PosixVirtualAligner aligner;
 
 uint64_t ProfileContains(int depth);
+uint64_t ProfileSequentialAdds(int count);
+uint64_t ProfileSequentialRemoves(int count);
 
 int main() {
+  // TODO: check the maximum depth we can do on this platform
   for (int depth = 2; depth <= 20; depth += 2) {
     assert(aligner.GetAllocCount() == 0);
     std::cout << "AvlTree<int>::Contains() [depth = " << depth << "] ... "
       << std::flush << ProfileContains(depth) << " nanos" << std::endl;
+  }
+  for (int factor = 0; factor < 4; ++factor) {
+    int count = 0x800 << factor;
+    std::cout << "AvlTree<int>::Add() [sequential, " << count << "] ... "
+      << std::flush << ProfileSequentialAdds(count) << std::endl;
+    std::cout << "AvlTree<int>::Remove() [sequential, " << count << "] ... "
+      << std::flush << ProfileSequentialRemoves(count) << std::endl;
   }
   return 0;
 }
@@ -44,4 +55,40 @@ uint64_t ProfileContains(int depth) {
     __asm__ __volatile__("" : : "r" (tree.Contains(element)));
   }
   return (Nanotime() - start) / iterations;
+}
+
+uint64_t ProfileSequentialAdds(int count) {
+  StackAllocator<sizeof(AvlTree<int>::Node)> stack(count, aligner);
+  AvlTree<int> tree(stack);
+  
+  const int iterations = 100;
+  uint64_t total = 0;
+  for (int i = 0; i < iterations; ++i) {
+    uint64_t start = Nanotime();
+    for (int j = 0; j < count; ++j) {
+      tree.Add(j);
+    }
+    total += Nanotime() - start;
+    tree.Clear();
+  }
+  return total / iterations;
+}
+
+uint64_t ProfileSequentialRemoves(int count) {
+  StackAllocator<sizeof(AvlTree<int>::Node)> stack(count, aligner);
+  AvlTree<int> tree(stack);
+  
+  const int iterations = 100;
+  uint64_t total = 0;
+  for (int i = 0; i < iterations; ++i) {
+    for (int j = 0; j < count; ++j) {
+      tree.Add(j);
+    }
+    uint64_t start = Nanotime();
+    for (int j = 0; j < count; ++j) {
+      tree.Remove(j);
+    }
+    total += Nanotime() - start;
+  }
+  return total / iterations;
 }
---
+++ test/stack-allocator.hpp
@@ -0,0 +1,57 @@
+#ifndef __TEST_STACK_ALLOCATOR_HPP__
+#define __TEST_STACK_ALLOCATOR_HPP__
+
+#include <analloc2>
+
+template <size_t ObjSize>
+class StackAllocator : public analloc::VirtualAllocator {
+public:
+  StackAllocator(size_t _capacity, analloc::VirtualAllocator & _source)
+      : source(_source),
+        capacity(_capacity),
+        buffers(new uintptr_t[_capacity]) {
+    for (currentCount = 0; currentCount < capacity; ++currentCount) {
+      if (!source.Alloc(buffers[currentCount], ObjSize)) {
+        break;
+      }
+    }
+  }
+  
+  virtual ~StackAllocator() {
+    assert(capacity == currentCount);
+    for (size_t i = 0; i < capacity; ++i) {
+      source.Dealloc(buffers[i], ObjSize);
+    }
+    delete[] buffers;
+  }
+  
+  virtual bool Alloc(uintptr_t & out, size_t size) {
+    if (size > ObjSize) return false;
+    if (!currentCount) return false;
+    out = buffers[--currentCount];
+    return true;
+  }
+  
+  virtual bool Realloc(uintptr_t &, size_t newSize) {
+    if (newSize > ObjSize) return false;
+    return true;
+  }
+  
+  virtual void Free(uintptr_t ptr) {
+    assert(currentCount < capacity);
+    buffers[currentCount++] = ptr;
+  }
+  
+  virtual void Dealloc(uintptr_t ptr, size_t size) {
+    assert(size <= ObjSize);
+    Free(ptr);
+  }
+  
+private:
+  analloc::VirtualAllocator & source;
+  size_t capacity;
+  size_t currentCount;
+  uintptr_t * buffers;
+};
+
+#endif
