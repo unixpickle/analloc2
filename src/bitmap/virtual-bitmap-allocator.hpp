94fc4971b0b544605f431b0d0a98e2a0aa45531e
Alex Nichol <unixpickle@gmail.com>
2014-10-18 11:29:00 -0400

    lots of internal docs in VirtualBitmapAllocator

--- src/bitmap/virtual-bitmap-allocator.hpp
+++ src/bitmap/virtual-bitmap-allocator.hpp
@@ -20,13 +20,76 @@ public:
   /**
    * Create a [VirtualBitmapAllocator] which is embedded in a region of memory.
    * 
-   * Returns `nullptr` if there is literally not enough space to generate a
+   * Returns `nullptr` if there is not enough space to generate a
    * [VirtualBitmapAllocator] object with the proper page size and unit
    * alignments.
    */
+  static VirtualBitmapAllocator<Unit> * Place(uintptr_t region, size_t size,
+                                              size_t page =
+                                                  sizeof(uintptr_t)) {
+    return PlaceObject(region, size, page);
+  }
+  
+  VirtualBitmapAllocator(size_t pageSize, uintptr_t _offset,
+                         Unit * ptr, size_t size)
+      : super(pageSize, pageSize, _offset, ptr, size / pageSize) {}
+  
+protected:
+  /**
+   * Computes the optimal bitmap size to cover a region of memory.
+   *
+   * Returns the size of the bitmap in bytes. The bitmap size will be aligned
+   * both by sizeof(Unit) and by [pageSize].
+   *
+   * Sets [freeSize] to the number of bytes which can be covered by the
+   * allocator. This may not be usableSize - bitmapSize, in the case where the
+   * optimal bitmap doesn't have enough bits to cover the entire buffer.
+   */
+  static size_t BestBitmapSize(size_t usableSize, size_t pageSize,
+                               size_t & freeSize) {
+    size_t align = ansa::Align(sizeof(Unit), pageSize);
+    if (align >= usableSize) {
+      freeSize = 0;
+      return 0;
+    }
+    
+    // Use basic division to find a lower bound for the bitmap size.
+    size_t byteCount = ansa::Align(usableSize / (pageSize * 8 + 1), align);
+    size_t result = byteCount;
+    freeSize = FreeSizeForBitmap(usableSize, pageSize, byteCount);
+    
+    // Attempt to round up and see if we can represent more space.
+    if (!ansa::AddWraps<size_t>(byteCount, align) &&
+        byteCount + align < usableSize) {
+      size_t f = FreeSizeForBitmap(usableSize, pageSize, byteCount + align);
+      if (f > freeSize) {
+        freeSize = f;
+        result = byteCount + align;
+      }
+    }
+    return result;
+  }
+  
+  /**
+   * Compute the maximum amount of data that can be covered by a bitmap in a
+   * given region.
+   */
+  static size_t FreeSizeForBitmap(size_t usableSize, size_t pageSize,
+                                  size_t bitmapSize) {
+    assert(ansa::IsAligned(bitmapSize, ansa::Align(sizeof(Unit), pageSize)));
+    size_t unitCount = bitmapSize / sizeof(Unit);
+    size_t representable = pageSize * unitCount * sizeof(Unit) * 8;
+    return ansa::Min(usableSize - bitmapSize, representable);
+  }
+  
+  /**
+   * Internal implementation of [Place].
+   *
+   * The template argument allows subclasses to place themselves in regions of
+   * memory, even if they require more storage than [VirtualBitmapAllocator].
+   */
   template <class T = VirtualBitmapAllocator<Unit> >
-  static T * Place(uintptr_t region, size_t size,
-                   size_t page = sizeof(uintptr_t)) {
+  static T * PlaceObject(uintptr_t region, size_t size, size_t page) {
     size_t align = ansa::Align(sizeof(Unit), page);
     
     // Compute the number of bytes we have for the bitmap and buffers combined.
@@ -47,43 +110,6 @@ public:
     uintptr_t offset = region + structureSize + bitmapSize;
     
     return new(ptr) T(page, offset, buffer, freeSize);
-  }
-  
-  VirtualBitmapAllocator(size_t pageSize, uintptr_t _offset,
-                         Unit * ptr, size_t size)
-      : super(pageSize, pageSize, _offset, ptr, size / pageSize) {}
-  
-protected:
-  static size_t BestBitmapSize(size_t usableSize, size_t pageSize,
-                               size_t & freeSize) {
-    size_t align = ansa::Align(sizeof(Unit), pageSize);
-    if (align >= usableSize) {
-      freeSize = 0;
-      return 0;
-    }
-    
-    // Use basic division to find a lower bound for the bitmap size.
-    size_t byteCount = ansa::Align(usableSize / (pageSize * 8 + 1), align);
-    size_t result = byteCount;
-    freeSize = FreeSizeForBitmap(usableSize, pageSize, byteCount);
-    // Attempt to round up and see if we can represent more space.
-    if (!ansa::AddWraps<size_t>(byteCount, align) &&
-        byteCount + align < usableSize) {
-      size_t f = FreeSizeForBitmap(usableSize, pageSize, byteCount + align);
-      if (f > freeSize) {
-        freeSize = f;
-        result = byteCount + align;
-      }
-    }
-    return result;
-  }
-  
-  static size_t FreeSizeForBitmap(size_t usableSize, size_t pageSize,
-                                  size_t bitmapSize) {
-    assert(ansa::IsAligned(bitmapSize, ansa::Align(sizeof(Unit), pageSize)));
-    size_t unitCount = bitmapSize / sizeof(Unit);
-    size_t representable = pageSize * unitCount * sizeof(Unit) * 8;
-    return ansa::Min(usableSize - bitmapSize, representable);
   }
 };
 
