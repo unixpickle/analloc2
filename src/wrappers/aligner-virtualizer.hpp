b20580549afd1a86420ce4aeb651aadeffa6e139
Alex Nichol <unixpickle@gmail.com>
2014-10-23 06:15:46 -0400

    drafted AlignerVirtualizer

--- src/wrappers/aligner-virtualizer.hpp
+++ src/wrappers/aligner-virtualizer.hpp
@@ -10,12 +10,27 @@ class AlignerVirtualizer
     : public AllocatorVirtualizer<T>,
       public virtual VirtualOffsetAligner {
 public:
+  typedef AllocatorVirtualizer<T> super;
+  
+  template <typename... Args>
+  AlignerVirtualizer(Args... args) : super(args...) {}
+  
   bool Align(uintptr_t & addressOut, uintptr_t align, size_t size) {
-    // TODO: this
+    return this->OffsetAlign(addressOut, align, 0, size);
   }
   
-  bool OffsetAlign(uintptr_t & addressOut, uintptr_t align, size_t size) {
-    // TODO: this
+  bool OffsetAlign(uintptr_t & addressOut, uintptr_t align, uintptr_t offset,
+                   size_t size) {
+    uintptr_t buffer;
+    if (!T::OffsetAlign(buffer, align, offset + this->headerSize, size)) {
+      return false;
+    }
+    // Set the size in the header
+    Header * header = (Header *)buffer;
+    header->size = size;
+    // Return the part of the buffer after the header
+    addressOut = buffer + this->headerSize;
+    return true;
   }
 };
 
