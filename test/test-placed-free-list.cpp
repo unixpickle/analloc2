5ae5871bf32c2e3145e659508b82e212dfe00346
Alex Nichol <unixpickle@gmail.com>
2014-11-01 17:07:14 -0400

    both alloc overflow cases pass

--- test/test-placed-free-list.cpp
+++ test/test-placed-free-list.cpp
@@ -1,6 +1,6 @@
 /**
  * Throughout these tests, you might see some notation indicating the state of
- * a placement allocator. General things to know:
+ * an allocator. General things to know:
  *
  * - R = active region structure in memory
  * - S = inactive region structure in the stack
@@ -8,6 +8,10 @@
  *
  * I denote disjoint regions using brackets ([ and ]). I separate cells of
  * memory using vertical bars (|).
+ *
+ * Most of the time, I used this notation to figure out what a test should do.
+ * A lot of the time, I assumed a large stack capacity so that I could see the
+ * inductive nature of the test.
  */
 
 #include "scoped-pass.hpp"
@@ -127,15 +131,116 @@ void TestOffsetPlace() {
 
 template <size_t Capacity>
 void TestNormalAllocOverflow() {
+  ScopedPass pass("PlacedFreeList<", Capacity, ">::Alloc() [single overflow]");
   
+  typedef PlacedFreeList<Capacity> Pfl;
+  
+  uintptr_t addr;
+  
+  size_t preambleSize = ComputePreambleSize<Capacity>();
+  size_t regionSize = ComputeRegionSize<Capacity>();
+  
+  // Let R=regionSize, N=Capacity
+  // We will have:
+  // - one region of size R*N
+  // - a region of size 5*R
+  // - N-2 regions of size 2*R
+  // - a region of size R
+  // - a region of size 2*R
+  // - Every region will be separated by R bytes
+  
+  size_t headingSize = Capacity * regionSize;
+  size_t firstSize = 5 * regionSize;
+  size_t blockSize = 2 * regionSize;
+  size_t totalSize = preambleSize + headingSize + firstSize +
+      blockSize * (Capacity - 1) + (Capacity + 2) * regionSize;
+  
+  void * buffer;
+  assert(!posix_memalign(&buffer, regionSize, totalSize));
+  
+  Pfl * pfl = Pfl::Place((uintptr_t)buffer, preambleSize + headingSize);
+  assert(pfl != nullptr);
+  assert(pfl->GetStackCount() == 1);
+  
+  uintptr_t start = (uintptr_t)buffer + preambleSize;
+  
+  // Currently, the allocator looks like this: [S|R| |...]
+  
+  pfl->Dealloc(start + headingSize + regionSize * 3, regionSize * 3);
+  
+  // Now, the allocator looks like this:
+  // [R|R|S| |...]    [ |...]
+  
+  // Now, deallocate enough more regions to saturate the heading
+  for (size_t i = 0; i < Capacity - 3; ++i) {
+    assert(pfl->GetStackCount() == 1);
+    size_t offset = headingSize + regionSize * 6 + blockSize * i +
+        regionSize * (i + 1);
+    pfl->Dealloc(start + offset, blockSize);
+  }
+  // Ensure that the heading was saturated
+  assert(pfl->GetStackCount() == 2);
+  
+  // The allocator now looks like this:
+  // [R|S|R|...|S]    [ | | ] [ | ] ...
+  
+  // Now, we unsaturate the heading by deallocating another block
+  size_t unsaturateOffset = headingSize + regionSize * 6 +
+      blockSize * (Capacity - 3) + regionSize * (Capacity - 2);
+  pfl->Dealloc(start + unsaturateOffset, blockSize);
+  
+  // The allocator now looks like this:
+  // [R|R|R|...|S]    [ | | ] [ | ] ...
+  
+  // Add a small block to the allocator to fill up the heading altogether
+  size_t smallBlockOffset = unsaturateOffset + regionSize * 3;
+  pfl->Dealloc(start + smallBlockOffset, regionSize);
+  
+  // The allocator now looks like this:
+  // [R|R|R|...|R]    [S| | ] [ | ] ... [ ]
+  
+  // Throw another block on the end.
+  size_t lastBlockOffset = smallBlockOffset + regionSize * 2;
+  assert(lastBlockOffset + regionSize * 2 == totalSize - preambleSize);
+  pfl->Dealloc(start + lastBlockOffset, blockSize);
+  
+  // The allocator now looks like this:
+  // [R|R|R|...|R]    [R|S| ] [ | ] ... [ ] [ | ]
+  
+  // Ninja the hell out of the allocator by adding a region below the first
+  // region.
+  pfl->Dealloc(start + headingSize + regionSize, 2 * regionSize);
+  
+  // The allocator now looks like this:
+  // [R|R|R|...|R] [S| |R|R| ] [ | ] ... [ ] [ | ]
+  
+  // The next N-2 allocations will add to the stack
+  for (size_t i = 0; i < Capacity - 2; ++i) {
+    size_t offset = headingSize + regionSize * 6 + blockSize * i +
+        regionSize * (i + 1);
+    assert(pfl->Alloc(addr, blockSize));
+    assert(addr == start + offset);
+    assert(pfl->GetStackCount() == i + 2);
+  }
+  
+  // The allocator now looks like this:
+  // [R|S|...|S|R] [S| |R|R| ] [ ] [ | ]
+  
+  // Now, we'll deallocate the last block and go over the maximum stack size.
+  assert(pfl->Alloc(addr, blockSize));
+  assert(addr == start + lastBlockOffset);
+  assert(pfl->GetStackCount() == Capacity - 1);
+  
+  // The allocator now looks like this:
+  // [R|S|...|S|R] [S| | |R| ] [ ] [ | ]
+  
+  // We should now be able to allocate 2*R and find it in the first block
+  assert(pfl->Alloc(addr, regionSize * 2));
+  assert(addr == start + headingSize + regionSize * 2);
 }
 
 template <size_t Capacity>
 void TestDoubleAllocOverflow() {
-  // NOTE: most of the visual examples in this test make use of a large
-  // Capacity to give a good depiction of what's going on. This test works for
-  // Capacity >= 3, but the visual examples don't.
-  
   ScopedPass pass("PlacedFreeList<", Capacity, ">::Alloc() [double overflow]");
   
   typedef PlacedFreeList<Capacity> Pfl;
